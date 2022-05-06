819486415757842a322698a9f18ebcae6c87f2b3
RockLeon <34214497+rockleona@users.noreply.github.com>
2022-05-06 12:44:06 +0800

    fix ignore

--- tester.ts
+++ tester.ts
@@ -12,14 +12,21 @@ export const tokenGen = () => {
   return unsecuredJwt;
 };
 
-export const tokenDec = (jwtPayload : string) =>{
-    const payload = jose.UnsecuredJWT.decode(jwtPayload, {
-        issuer: "snail-journey",
-        audience: "snail-journey-players"
-    });
-    console.log(payload);
-    console.log(payload.payload['urn:example:claim']);
-}
+export const tokenDec = (jwtPayload: string) => {
+  const payload = jose.UnsecuredJWT.decode(jwtPayload, {
+    issuer: "snail-journey",
+    audience: "snail-journey-players",
+  });
+  console.log(payload);
+  console.log(payload.payload["urn:example:claim"]);
+  
+  if(payload.payload["exp"] != undefined){
+    const time = new Date( new Date(0).setUTCSeconds(payload.payload["exp"]));
+    console.log(time.getTime());
+    console.log(Date.now());
+  }
+};
 
 const jwt = tokenGen();
+// const jwt = 'eyJhbGciOiJub25lIn0.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjUxODA4MDYzLCJpc3MiOiJzbmFpbC1qb3VybmV5IiwiYXVkIjoic25haWwtam91cm5leS1wbGF5ZXJzIiwiZXhwIjoxNjUxODE1MjYzfQ.'
 tokenDec(jwt);
